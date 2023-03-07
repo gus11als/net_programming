e02ab6755c2c3eafda82829899e668c81a8ec658
Hyeonmin Kim <gus12als@gmail.com>
2023-03-07 11:06:33 +0900

    HW2

---
+++ HW2_HYEONMIN_KIM/HW2.py
@@ -0,0 +1,11 @@
+str = 'My name is Hyeon min Kim'
+print(len(str))
+print(str*10)
+print(str[0])
+print(str[0:4])
+print(str[-4:])
+print(str[::-1]) #거꾸로 출력
+print(str[1:-1])     #처음과 마지막 제거
+print(str.upper())
+print(str.lower())
+print(str.replace('a','e'))
